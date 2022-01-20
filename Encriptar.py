b894559c3c2b2f5df65ebee90327f56308ce309c
beatrizmotta <62359366+beatrizmotta@users.noreply.github.com>
2022-01-20 17:42:55 -0300

    Tentativa de encriptar com espaço + símbolos

--- Encriptar.py
+++ Encriptar.py
@@ -1,12 +1,43 @@
 def encriptar(escolhaInicial):
 
-    mensagem = input("Digite o texto que você quer encriptar: ")
-    cp = int(input("Digite a sua chave pública: ")
-    mensagemEncriptada = [ord(x) - 95 for x in mensagem]
+    # Criando uma lista com alguns caracteres da tabela ASCII (alfabeto + alguns símbolos)
+    alphabet = list()
+    for i in range(32, 800):
+        alphabet.append(chr(i))
 
-    novaMsgmEncrip = str(mensagemEncriptada)
+    # Pede o texto para o usuário
+    mensagem = input("Digite o texto que você quer encriptar: ")
+
+    # Codifica a mensagem
+    def codify(char):
+        index = 0
+        codified = list()
+
+        # Vai letra por letra da mensagem e vê se ela existe na lista que criamos anteriormente
+        for i in range(0, len(char)):
+            for k in range(0, len(alphabet)):
+                # Caso não exista, insere o caractere como ele está 
+                if (char[i] not in alphabet):
+                    codified.append(char[i])
+                    break
+                else:
+                    # Caso exista, insere o caractere transposto 5 caracteres para a frente 
+                    if (char[i] == alphabet[k]):
+                        index = k
+                        codified.append(alphabet[(index + 5) % 768])
+                        # 768 = 800 - 32, o número de caracteres que temos no 'alfabeto' que criamos na lista
+                        
+        return codified;
     
+    # Falta integrar aqui a parte da chave pública
+    # cp = int(input("Digite a sua chave pública: "))
+    mensagemEncriptada = codify(mensagem)
+    mensagemEncriptada = ''.join(codify(mensagemEncriptada))
+    print(mensagemEncriptada)
+
     arquivo = open("arquivo.txt", "a")
-    arquivo.writelines(novaMsgmEncrip + '\n')
+    arquivo.writelines(mensagemEncriptada)
     
-    print("Sua mensagem encriptada é:", novaMsgmEncrip)
+    print("Sua mensagem encriptada é:", mensagemEncriptada)
+
+encriptar(1)
