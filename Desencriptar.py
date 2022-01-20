c7a488497cbe1010b1448e11edd5ce540a4fd03c
beatrizmotta <62359366+beatrizmotta@users.noreply.github.com>
2022-01-20 20:15:31 -0300

    Update Desencriptar.py

--- Desencriptar.py
+++ Desencriptar.py
@@ -1,3 +1,42 @@
 def desencriptar(escolhaInicial):
 
-    print('issoai')
+    # Criando uma lista com alguns caracteres da tabela ASCII (alfabeto + alguns símbolos) para poder fazer 
+    # a decodificação 
+
+    alphabet = list()
+    for i in range(32, 127):
+        alphabet.append(chr(i))
+
+    # Pede o texto para o usuário
+    mensagem = input("Digite o texto que você quer desencriptar: ")
+
+    # Decodifica a mensagem
+    def decodify(char):
+        index = 0
+        decodified = list()
+        # Vai letra por letra da mensagem e vê se ela existe na lista que criamos anteriormente
+        for i in range(0, len(char)):
+            for k in range(0, len(alphabet)):
+                # Caso não exista, insere o caractere como ele está 
+                if (char[i] not in alphabet):
+                    decodified.append(char[i])
+                    break
+                else:
+                    # Caso exista, insere o caractere transposto 5 caracteres para a trás
+                    if (char[i] == alphabet[k]):
+                        index = k
+                        decodified.append(alphabet[(index - 5) % 95])
+                    
+        return decodified;
+    
+    # cp = int(input("Digite a sua chave pública: "))
+    mensagemDesencriptada = decodify(mensagem)
+    mensagemDesencriptada = ''.join(decodify(mensagemDesencriptada))
+
+    arquivo = open("arquivo.txt", "a")
+    arquivo.writelines(mensagemDesencriptada)
+    
+    print("Sua mensagem desencriptada é:", mensagemDesencriptada)
+
+
+desencriptar(2)
